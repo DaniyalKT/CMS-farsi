bbaf0bb9c89d3ac9fda183c868b9a98eca365f83
دانیال کفائی نژاد طهرانی <65863212+DaniyalKT@users.noreply.github.com>
2024-03-13 22:38:30 +0330

    deletemodal actions

--- src/Component/DeleteModal/DeleteModal.js
+++ src/Component/DeleteModal/DeleteModal.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./DeleteModal.css";
 import ReactDOM from "react-dom";
 
-function DeleteModal() {
+function DeleteModal({isShowDelete , SubmitAction , CancelAction}) {
   return ReactDOM.createPortal(
-    <div className="modal-parent">
+    <div className={isShowDelete ? "modal-parent active": 'modal-parent'}>
       <div className="delete-modal">
         <h1 className="">آیا از حذف اطمینان دارید؟</h1>
         <div className="delete-modal-btns">
-          <button className="delete-btn delete-modal-accept-btn">بله</button>
-          <button className="delete-btn delete-modal-reject-btn">خیر</button>
+          <button className="delete-btn delete-modal-accept-btn" onClick={SubmitAction}>بله</button>
+          <button className="delete-btn delete-modal-reject-btn" onClick={CancelAction}>خیر</button>
         </div>
       </div>
     </div>,
--- src/Component/ProductsTable/ProductsTable.js
+++ src/Component/ProductsTable/ProductsTable.js
@@ -1,34 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductsTable.css";
+import DeleteModal from "../DeleteModal/DeleteModal";
 
 function ProductsTable() {
-  return (
-    <table className="products-table">
-      <tr className="products-table-heading-tr">
-        <th>عکس</th>
-        <th>اسم</th>
-        <th>قیمت</th>
-        <th>موجودی</th>
-      </tr>
+  const [isShowDelete, setIsShowDelete] = useState(false);
 
-      <tr className="products-table-tr">
-        <td>
-          <img
-            src="images/oil.jpg"
-            alt="oil Image"
-            className="products-table-img"
-          />
-        </td>
-        <td>روغن سرخ کردنی</td>
-        <td>92000 تومان</td>
-        <td>82</td>
-        <td>
-          <button className="products-table-btn">جزئیات</button>
-          <button className="products-table-btn">حذف</button>
-          <button className="products-table-btn">ویرایش</button>
-        </td>
-      </tr>
-    </table>
+  const deleteModalCandelHandler = () => {
+    console.log('دیلیت کنسل شد')
+    setIsShowDelete(false);
+  };
+  const deleteModalSubmitHandler = () => {
+    console.log('دیلیت تایید شد')
+    setIsShowDelete(false);
+  };
+  
+  return (
+    <>
+      <table className="products-table">
+        <thead>
+          <tr className="products-table-heading-tr">
+            <th>عکس</th>
+            <th>اسم</th>
+            <th>قیمت</th>
+            <th>موجودی</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="products-table-tr">
+            <td>
+              <img
+                src="images/oil.jpg"
+                alt="oil Image"
+                className="products-table-img"
+              />
+            </td>
+            <td>روغن سرخ کردنی</td>
+            <td>92000 تومان</td>
+            <td>82</td>
+            <td>
+              <button className="products-table-btn">جزئیات</button>
+              <button className="products-table-btn" onClick={()=> setIsShowDelete(true)}>حذف</button>
+              <button className="products-table-btn">ویرایش</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <DeleteModal isShowDelete={isShowDelete} SubmitAction={deleteModalSubmitHandler} CancelAction={deleteModalCandelHandler}/>
+    </>
   );
 }
 
